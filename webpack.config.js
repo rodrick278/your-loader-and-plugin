e7224b6c76b98eb9829fac20902a0075ed91831f
rodrick <jckmyemail@163.com>
2021-05-20 20:44:51 +0800

    up

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const OutLogPlugin = require('./plugins/OutLogPlugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -23,11 +24,14 @@ module.exports = {
       },
     ],
   },
-  plugins: [],
+  plugins: [
+    new OutLogPlugin({outFileName:"buildInfo"})
+  ],
   resolveLoader: {
     // 去哪些目录下寻找 Loader，有先后顺序之分 node_modules 如果没有就去./loaders目录下
     modules: ['node_modules', './loaders/'],
   },
   // 打包模式，开发||生产
   mode: 'development',
+  devtool: 'cheap-source-map'
 }
